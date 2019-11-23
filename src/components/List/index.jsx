ad2d140914f32e5ead7ebb95a21b2f34038ca22c
Murad <murad-arslangiraev-1994@mail.ru>
2019-11-24 01:02:08 +0300

    Нашел в чем ошибка, разобрался.)

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -8,38 +8,38 @@ import Badge from '../Badge';
 
 import './List.sass';
 
-const List = ({items, isRemoVable, onClick, onRemove}) => {
+const List = ({items, isRemovable, onClick, onRemove}) => {
     const removeList = (item) => {
-      if(window.confirm('Вы действительно хотите удалить список?')) {
-        axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
-          // удаление сущности по id
-          onRemove(item.id);
-          // Функция удаления
-        });
-      }
+        if(window.confirm('Вы действительно хотите удалить список?')) {
+            axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
+            // удаление сущности по id
+            onRemove(item.id);
+            // Функция удаления
+            });
+        }
     }
-
 
     return (
         <ul onClick = {onClick} className="list">
-          {items.map((item, index) => (
+            {items.map((item, index) => (
                 <li 
-                  key={index} 
-                  className={classNames(item.className, { active: item.active})}
+                    key={index} 
+                    className={classNames(item.className, { active: item.active})}
                 >
-                <i>{item.icon ? item.icon : <Badge color={item.color.name}/>}</i>
-                <span>{item.name}</span>
-                {isRemoVable && (
-                  <img className='list-remove__icon' 
-                      src={removeSvg}
-                      alt="Remove icon" 
-                      onClick={() => removeList(item)} 
-                  />
-                  )}
+                    <i>{item.icon ? item.icon : <Badge color={item.color.name}/>}</i>
+                    <span>{item.name}</span>
+                    {isRemovable && (
+                        <img 
+                            className='list-remove__icon' 
+                            src={removeSvg}
+                            alt="Remove icon" 
+                            onClick={() => removeList(item)} 
+                        />
+                    )}
                 </li>
             ))}
         </ul>
-    )
-}
+    );
+};
 
 export default List;
--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -5,6 +5,7 @@ import './Tasks.sass'
 
 
 const Tasks = ({list}) => {
+    console.log(list);
     return (
         <div className='tasks'>
             <h2 className='tasks-title'>
@@ -24,13 +25,12 @@ const Tasks = ({list}) => {
                                     viewBox="0 0 11 8" 
                                     fill="none" 
                                     xmlns="http://www.w3.org/2000/svg">
-
-                                    <path 
-                                        d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" 
-                                        stroke="#000" 
-                                        stroke-width="1.5" 
-                                        stroke-linecap="round" 
-                                        stroke-linejoin="round"/>
+                                <path 
+                                    d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" 
+                                    stroke="#000" 
+                                    strokeWidth="1.5" 
+                                    strokeLinecap="round" 
+                                    strokeLinejoin="round"/>
                                 </svg>
                             </label>
                         </div>
